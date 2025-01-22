1047476fa542b06dbd3442ddf3a67c8da5eaf008
Crackheadllc <DONGEEZYBABA1@GMAIL.COM>
2025-01-21 22:02:04 -0800

    DONE

--- src/main/java/org/example/week2/d4/e5/E5PizzaPartyModulus.java
+++ src/main/java/org/example/week2/d4/e5/E5PizzaPartyModulus.java
@@ -3,11 +3,20 @@ package org.example.week2.d4.e5;
 public class E5PizzaPartyModulus {
     public static void main(String[] args) {
         // Declare the int variables and assign values
-        int totalSlices=13;
-        int friends=4;
+        int totalSlices = 13;
+        int friends = 4;
 
 
         // Calculate slices per person
-        int slicesPerPerson;
-        System.out.println("slicesPerPerson" + "div"+ (totalSlices+friends));
+        int slicesPerPerson = totalSlices / friends;
+        int leftoverSlices = totalSlices % friends;
+
+
+        //Print the results
+
+        System.out.println("Each person gets " +slicesPerPerson+ " slices. ");
+        System.out.println("There are " +leftoverSlices+ " slices left over.");
+
+    }
+}
 
