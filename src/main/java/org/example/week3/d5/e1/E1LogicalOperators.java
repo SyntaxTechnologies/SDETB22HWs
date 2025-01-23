09a5b266fb14dff5a7bb89b6f2e382d0c914f23a
Asghar Nazir <asghar@syntaxtechs.com>
2025-01-23 13:42:23 -0500

    Week3 D5

---
+++ src/main/java/org/example/week3/d5/e1/E1LogicalOperators.java
@@ -0,0 +1,25 @@
+package org.example.week3.d5.e1;
+
+public class E1LogicalOperators {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+
+        // Print prompt for user to answer if they need a loan
+        // Capture the loan requirement input
+
+        // Check if the user needs a loan
+
+        // Use conditional statements to determine the user's loan eligibility based on their credit score:
+        // Set eligibility to "Not eligible",if creditScore is below 600.
+        // Set eligibility to "Maybe eligible",if creditScore is between 600 and 700 inclusive.
+        // Set eligibility to "Eligible",if creditScore is between 701 and 800 inclusive.
+        // Set eligibility to "Definitely eligible",if creditScore is higher than 800.
+        // If needsLoan is false, set eligibility to "Unknown".
+        // Print Eligibility
+        // Print "The eligibility is ____" with the corresponding eligibility status
+
+
+
+
+    }
+}
